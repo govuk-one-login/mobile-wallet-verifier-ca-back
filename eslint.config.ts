2d341a9590004883b1ca1641eae281ffe2ebf8e7
Richa Misra <178929731+richa-misra-gds@users.noreply.github.com>
2025-12-09 13:19:08 +0000

    liniting

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,17 @@ import tseslint from 'typescript-eslint';
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  { ignores: ['**/*.js', 'node_modules/**/*', '.aws-sam/**/*', 'coverage/**/*', '.prettierrc.cjs', 'dist/**/*', 'build.mjs'] },
+  {
+    ignores: [
+      '**/*.js',
+      'node_modules/**/*',
+      '.aws-sam/**/*',
+      'coverage/**/*',
+      '.prettierrc.cjs',
+      'dist/**/*',
+      'build.mjs',
+    ],
+  },
   {
     rules: {
       semi: 'error',
