311bfd8fcfd08e37c773fc5cd85c193d80705f51
James Mooney <178897394+jmooney-dd@users.noreply.github.com>
2026-02-17 16:32:35 +0000

    test: add custom matchers so we can test logs

---
+++ tests/matchers.ts
@@ -0,0 +1,73 @@
+import { expect, MockInstance } from 'vitest';
+
+const toHaveBeenCalledWithLogFields = (
+  consoleSpy: MockInstance,
+  logFields: Record<string, unknown>,
+) => {
+  const messages = consoleSpy.mock.calls.map((args) => args[0]);
+  const pass = messages.some((message) => {
+    if (typeof message !== 'string') {
+      return false;
+    }
+
+    try {
+      const messageAsObject = JSON.parse(message);
+      return isSubsetOf(logFields, messageAsObject);
+    } catch {
+      return false;
+    }
+  });
+  return {
+    pass,
+    message: pass
+      ? () =>
+          `Expected not to find any log messages matching the specified fields and values: ${JSON.stringify(
+            logFields,
+          )}`
+      : () =>
+          `Expected to find at least one log message matching the specified fields and values: ${JSON.stringify(
+            logFields,
+          )}`,
+  };
+};
+
+function isSubsetOf(
+  object: Record<string, unknown>,
+  targetObject: Record<string, unknown>,
+): boolean {
+  return Object.keys(object).every((key) => {
+    if (object[key] instanceof Object && targetObject[key] instanceof Object) {
+      const objVal = object[key] as Record<string, unknown>;
+      const targetObjVal = targetObject[key] as Record<string, unknown>;
+      return isSubsetOf(objVal, targetObjVal);
+    }
+    return deepEquals(object[key], targetObject[key]);
+  });
+}
+
+function deepEquals(subject: unknown, target: unknown): boolean {
+  return JSON.stringify(subject) === JSON.stringify(target);
+}
+
+expect.extend({
+  toHaveBeenCalledWithLogFields,
+});
+
+interface CustomMatchers<R = unknown> {
+  toHaveBeenCalledWithLogFields: (logFields: object) => R;
+  toBeValidUuid: () => R;
+  toBeValidJwt: () => R;
+  toHaveUniqueElements: () => R;
+}
+
+declare module 'vitest' {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  interface Matchers<T = any> extends CustomMatchers<T> {
+    toHaveBeenCalledWithLogFields: (arg: {
+      messageCode?: string;
+      functionVersion?: string;
+      function_arn?: string;
+      testKey?: string;
+    }) => void;
+  }
+}
