e022871c2707b8502ef92f2f97ffcf24e51bafc5
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-02-17 15:10:06 +0000

    feat: add new handler for the issue reader cert lambda

---
+++ src/lambdas/issue-reader-cert-service/issue-reader-cert-service-handler.ts
@@ -0,0 +1,11 @@
+import type {
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult,
+    Context,
+} from 'aws-lambda';
+
+export const handler = async (
+    event: APIGatewayProxyEvent,
+    context: Context,
+): Promise<APIGatewayProxyResult> => {
+    }
