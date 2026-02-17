00d8626010b728d57613c762123c92aa5881155c
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-02-17 15:10:48 +0000

    feat: add logger

---
+++ src/lambdas/common/logMessage.ts
@@ -0,0 +1,17 @@
+import { LogAttributes } from "@aws-lambda-powertools/logger/types";
+
+export class LogMessage implements LogAttributes {
+
+    private constructor(
+        public readonly messageCode: string,
+        public readonly message: string,
+    ) {}
+
+    [key: string]: string; // Index signature needed to implement LogAttributes
+
+    static readonly ISSUE_READER_CERT_SERVICE_STARTED = new LogMessage(
+        "MOBILE_CA_ISSUE_READER_CERT_SERVICE_STARTED",
+        "Lambda handler processing has started.",
+    );
+
+}
