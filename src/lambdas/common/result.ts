2a35a6ed1023ce9514c48a80a5cee136caf1c2e2
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-02-17 16:32:33 +0000

    feat: add result util and test

---
+++ src/lambdas/common/result.ts
@@ -0,0 +1,61 @@
+export type Result<T, E = BaseError> = Success<T> | Failure<E>;
+
+type VoidType = ReturnType<() => void>;
+
+export type Success<T> = [T] extends [VoidType]
+    ? EmptySuccess
+    : SuccessWithValue<T>;
+export type Failure<E> = [E] extends [VoidType]
+    ? EmptyFailure
+    : FailureWithValue<E>;
+
+export type SuccessWithValue<T> = {
+    isError: false;
+    value: T;
+};
+
+export type EmptySuccess = {
+    isError: false;
+};
+
+export type FailureWithValue<E> = {
+    isError: true;
+    value: E;
+};
+
+export type EmptyFailure = {
+    isError: true;
+};
+
+export const successResult = <T>(value: T): SuccessWithValue<T> => {
+    return {
+        isError: false,
+        value,
+    };
+};
+
+export const emptySuccess = (): EmptySuccess => {
+    return {
+        isError: false,
+    };
+};
+
+export const errorResult = <E>(value: E): FailureWithValue<E> => {
+    return {
+        isError: true,
+        value,
+    };
+};
+
+export const emptyFailure = (): EmptyFailure => {
+    return {
+        isError: true,
+    };
+};
+
+export enum ErrorCategory {
+    SERVER_ERROR = "SERVER_ERROR",
+    CLIENT_ERROR = "CLIENT_ERROR",
+}
+
+type BaseError = { errorMessage: string; errorCategory?: ErrorCategory };
