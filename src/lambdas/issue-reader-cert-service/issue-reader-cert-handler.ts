72ec6d6eebf3534524dce41fb46159a32acd07be
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-02-18 11:05:51 +0000

    feat: insert dependencies into the lambda

--- src/lambdas/issue-reader-cert-service/issue-reader-cert-handler.ts
+++ src/lambdas/issue-reader-cert-service/issue-reader-cert-handler.ts
@@ -2,11 +2,14 @@ import type { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { setupLogger } from '../common/logger/setup-logger';
 import { LogMessage } from '../common/logger/log-message';
 import { logger } from '../common/logger/logger';
+import {IssueReaderCertDependencies} from "./handler-dependencies.ts";
 
 export const handler = async (
   _event: APIGatewayProxyEvent,
   context: Context,
+  dependencies: IssueReaderCertDependencies,
 ): Promise<void> => {
+
   setupLogger(context);
   logger.info(LogMessage.ISSUE_READER_CERT_STARTED);
 };
