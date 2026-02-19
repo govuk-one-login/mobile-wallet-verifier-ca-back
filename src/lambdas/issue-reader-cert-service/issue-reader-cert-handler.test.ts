99446668017a731566c894769f77f4eefc2c7a2c
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-02-19 11:40:28 +0000

    refactor: move buildRequest and buildLambdaEvent functions to testUtils folder

--- src/lambdas/issue-reader-cert-service/issue-reader-cert-handler.test.ts
+++ src/lambdas/issue-reader-cert-service/issue-reader-cert-handler.test.ts
@@ -8,6 +8,10 @@ import { handlerConstructor } from './issue-reader-cert-handler';
 import { logger } from '../common/logger/logger';
 import '../../../tests/testUtils/matchers';
 import { IssueReaderCertDependencies } from './handler-dependencies';
+import {
+  buildLambdaContext,
+  buildRequest,
+} from '../../../tests/testUtils/buildLambda.ts';
 
 let consoleInfoSpy: MockInstance;
 let consoleErrorSpy: MockInstance;
@@ -85,55 +89,3 @@ describe('Handler', () => {
     );
   });
 });
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function buildRequest(overrides?: any): APIGatewayProxyEvent {
-  const defaultRequest = {
-    httpMethod: 'get',
-    body: '',
-    headers: {
-      'x-correlation-id': 'mockCorrelationId',
-    },
-    isBase64Encoded: false,
-    multiValueHeaders: {},
-    multiValueQueryStringParameters: {},
-    path: '/mockPath',
-    pathParameters: {},
-    queryStringParameters: {},
-    requestContext: {
-      accountId: '123456789012',
-      apiId: '1234',
-      authorizer: {},
-      httpMethod: 'get',
-      identity: { sourceIp: '1.1.1.1' },
-      path: '/mockPath',
-      protocol: 'HTTP/1.1',
-      requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
-      requestTimeEpoch: 1428582896000,
-      resourceId: '123456',
-      resourcePath: '/mockPath',
-      stage: 'mockStage',
-    },
-    resource: '',
-    stageVariables: {},
-  };
-  return { ...defaultRequest, ...overrides };
-}
-function buildLambdaContext(): Context {
-  return {
-    callbackWaitsForEmptyEventLoop: true,
-    functionName: 'lambdaFunctionName',
-    functionVersion: '1',
-    invokedFunctionArn: 'arn:12345',
-    memoryLimitInMB: '1028',
-    awsRequestId: 'awsRequestId',
-    logGroupName: 'logGroup',
-    logStreamName: 'logStream',
-    getRemainingTimeInMillis: () => {
-      return 2000;
-    },
-    done: function (): void {},
-    fail: function (): void {},
-    succeed: function (): void {},
-  };
-}
