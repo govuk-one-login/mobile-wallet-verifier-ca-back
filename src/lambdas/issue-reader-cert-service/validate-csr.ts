ef0ad28487bb9ab1a8aa0ecb4504e5733f68a0c6
James Mooney <178897394+jmooney-dd@users.noreply.github.com>
2026-03-17 14:18:34 +0000

    refactor: extract validate-csr func to its own file

---
+++ src/lambdas/issue-reader-cert-service/validate-csr.ts
@@ -0,0 +1,77 @@
+import {
+  Pkcs10CertificateRequest,
+  BasicConstraintsExtension,
+} from '@peculiar/x509';
+import { LogMessage } from '../common/logger/log-message';
+import { logger } from '../common/logger/logger';
+import { Result, errorResult, emptySuccess } from '../common/result/result';
+
+const BASIC_CONSTRAINTS_OID = '2.5.29.19';
+
+export async function validateCSR(
+  csrPem: string,
+): Promise<Result<void, string>> {
+  let csr: Pkcs10CertificateRequest;
+  try {
+    csr = new Pkcs10CertificateRequest(csrPem);
+  } catch (error: unknown) {
+    const errorMessage = 'CSR not valid PKCS#10 request';
+    logger.error(LogMessage.ISSUE_READER_CERT_CSR_VALIDATION_FAILURE, {
+      errorMessage,
+      error,
+    });
+    return errorResult(errorMessage);
+  }
+
+  try {
+    const validSignedCsr = await csr.verify();
+    if (!validSignedCsr) {
+      const errorMessage = 'CSR self signature verification failed';
+      logger.error(LogMessage.ISSUE_READER_CERT_CSR_VALIDATION_FAILURE, {
+        errorMessage,
+      });
+      return errorResult(errorMessage);
+    }
+  } catch (error: unknown) {
+    const errorMessage = 'CSR self signature verification failed';
+    logger.error(LogMessage.ISSUE_READER_CERT_CSR_VALIDATION_FAILURE, {
+      errorMessage,
+      error,
+    });
+    return errorResult(errorMessage);
+  }
+
+  const csrPublicKeyAlgorithm = csr.publicKey.algorithm;
+  if (csrPublicKeyAlgorithm.name !== 'ECDSA') {
+    const errorMessage = 'CSR public key not EC key';
+    logger.error(LogMessage.ISSUE_READER_CERT_CSR_VALIDATION_FAILURE, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  if (
+    !('namedCurve' in csrPublicKeyAlgorithm) ||
+    csrPublicKeyAlgorithm.namedCurve !== 'P-256'
+  ) {
+    const errorMessage = 'CSR public key does not use P-256 curve';
+    logger.error(LogMessage.ISSUE_READER_CERT_CSR_VALIDATION_FAILURE, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  const basicConstraints = csr.getExtension(BASIC_CONSTRAINTS_OID);
+  if (
+    basicConstraints instanceof BasicConstraintsExtension &&
+    basicConstraints.ca
+  ) {
+    const errorMessage = 'CSR requests CA capabilities';
+    logger.error(LogMessage.ISSUE_READER_CERT_CSR_VALIDATION_FAILURE, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  return emptySuccess();
+}
