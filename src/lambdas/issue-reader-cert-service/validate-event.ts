852ac4f042916d3eb5bbc92a01ea713d52ed6ccf
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-03-18 14:58:17 +0000

    refactor: update validate event to have validateBody helper function

--- src/lambdas/issue-reader-cert-service/validate-event.ts
+++ src/lambdas/issue-reader-cert-service/validate-event.ts
@@ -4,71 +4,30 @@ import { errorResult, Result, successResult } from '../common/result/result.ts';
 import { logger } from '../common/logger/logger.ts';
 import { LogMessage } from '../common/logger/log-message.ts';
 
+interface ValidEventData {
+  firebaseAppCheckHeader: string;
+  csrPem: string;
+}
 export function validateEvent(
   eventHeaders: APIGatewayProxyEventHeaders,
   eventBody: string | null,
-): Result<{ firebaseAppCheckHeader: string; csrPem: string }, string> {
+): Result<ValidEventData, string> {
   const validateAppCheckHeaderResult =
     validateEventAppCheckHeader(eventHeaders);
   if (validateAppCheckHeaderResult.isError) {
     return validateAppCheckHeaderResult;
   }
+  const firebaseAppCheckHeader = validateAppCheckHeaderResult.value;
 
-  if (!eventBody) {
-    const errorMessage = 'Event body is null';
-    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
-      errorMessage,
-    });
-    return errorResult(errorMessage);
+  const validateEventBodyResult = validateEventBody(eventBody);
+  if (validateEventBodyResult.isError) {
+    return validateEventBodyResult;
   }
-
-  let parsedEventBody: unknown;
-  try {
-    parsedEventBody = JSON.parse(eventBody);
-  } catch {
-    const errorMessage = 'Event body cannot be parsed';
-    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
-      errorMessage,
-    });
-    return errorResult(errorMessage);
-  }
-  if (
-    typeof parsedEventBody !== 'object' ||
-    parsedEventBody === null ||
-    Array.isArray(parsedEventBody)
-  ) {
-    const errorMessage = 'Event body is not a JSON object';
-    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
-      errorMessage,
-    });
-    return errorResult(errorMessage);
-  }
-
-  if (!('csrPem' in parsedEventBody)) {
-    const errorMessage = 'Event body missing csrPem';
-    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
-      errorMessage,
-    });
-    return errorResult(errorMessage);
-  }
-  if (typeof parsedEventBody.csrPem !== 'string') {
-    const errorMessage = 'Event body csrPem is not a string';
-    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
-      errorMessage,
-    });
-    return errorResult(errorMessage);
-  }
-  if (!parsedEventBody.csrPem.trim()) {
-    const errorMessage = 'Event body csrPem is an empty string';
-    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
-      errorMessage,
-    });
-    return errorResult(errorMessage);
-  }
+  const csrPem = validateEventBodyResult.value;
 
   return successResult({
-    firebaseAppCheckHeader: validateAppCheckHeaderResult.value,
-    csrPem: parsedEventBody.csrPem,
+    firebaseAppCheckHeader,
+    csrPem,
   });
 }
 
@@ -88,4 +47,63 @@ function validateEventAppCheckHeader(
   }
 
   return successResult(firebaseAppCheckHeader);
+}
+
+function validateEventBody(eventBody: string | null): Result<string, string> {
+  if (!eventBody) {
+    const errorMessage = 'Event body is null';
+    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  let parsedEventBody: unknown;
+  try {
+    parsedEventBody = JSON.parse(eventBody);
+  } catch {
+    const errorMessage = 'Event body cannot be parsed';
+    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  if (
+    typeof parsedEventBody !== 'object' ||
+    parsedEventBody === null ||
+    Array.isArray(parsedEventBody)
+  ) {
+    const errorMessage = 'Event body is not a JSON object';
+    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  if (!('csrPem' in parsedEventBody)) {
+    const errorMessage = 'Event body missing csrPem';
+    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  if (typeof parsedEventBody.csrPem !== 'string') {
+    const errorMessage = 'Event body csrPem is not a string';
+    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  if (!parsedEventBody.csrPem.trim()) {
+    const errorMessage = 'Event body csrPem is an empty string';
+    logger.error(LogMessage.ISSUE_READER_CERT_INVALID_EVENT, {
+      errorMessage,
+    });
+    return errorResult(errorMessage);
+  }
+
+  return successResult(parsedEventBody.csrPem);
 }
