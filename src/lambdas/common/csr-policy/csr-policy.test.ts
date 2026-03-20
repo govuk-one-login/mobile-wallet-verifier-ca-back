a998162fa36c3e0c5017d1855f30eaab4a50f414
James Mooney <178897394+jmooney-dd@users.noreply.github.com>
2026-03-20 11:26:39 +0000

    test: add test for csr policy

---
+++ src/lambdas/common/csr-policy/csr-policy.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from 'vitest';
+import { BASIC_CONSTRAINTS_OID, CSR_SUBJECT_POLICY } from './csr-policy';
+
+describe('CSR Subject Policy', () => {
+  describe('BASIC_CONSTRAINTS_OID', () => {
+    it('Is "2.5.29.19"', () => {
+      expect(BASIC_CONSTRAINTS_OID).toEqual('2.5.29.19');
+    });
+  });
+
+  describe('CSR_SUBJECT_POLICY', () => {
+    describe('C', () => {
+      it('Is "GB"', () => {
+        expect(CSR_SUBJECT_POLICY.C).toEqual('GB');
+      });
+    });
+
+    describe('O', () => {
+      it('Is "Government Digital Service"', () => {
+        expect(CSR_SUBJECT_POLICY.O).toEqual('Government Digital Service');
+      });
+    });
+  });
+});
