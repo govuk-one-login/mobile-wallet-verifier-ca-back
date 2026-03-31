b0bb0068c4b104d96725c8243ec0e2ea30cc96b5
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-03-31 16:55:45 +0100

    refactor: add condition for test environment to be build

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eu
 
-if npm run test:integration; then
+if [[ "$TEST_ENVIRONMENT" == "build" ]]; then
+  if npm test:integration; then
     cp -rf results $TEST_REPORT_ABSOLUTE_DIR
-else
+  else
     cp -rf results $TEST_REPORT_ABSOLUTE_DIR
     exit 1
+  fi
 fi
