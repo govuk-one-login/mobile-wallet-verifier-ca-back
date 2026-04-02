b9ea948c517abde035b2d7e30308afbc746a8997
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-04-02 10:57:45 +0100

    fix: update condition

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 set -eu
 
 if [[ "$TEST_ENVIRONMENT" == "build" ]]; then
-  if npm test:integration; then
+  if npm run test:integration; then
     cp -rf results $TEST_REPORT_ABSOLUTE_DIR
   else
     cp -rf results $TEST_REPORT_ABSOLUTE_DIR
