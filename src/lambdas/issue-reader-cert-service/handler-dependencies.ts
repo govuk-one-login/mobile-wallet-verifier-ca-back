3f300afe30aac5aad2d53be1fa7f888751d9b3b3
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-04-13 12:22:38 +0100

    feat: add issueCertificate and getCertificate as hendler dependencies

--- src/lambdas/issue-reader-cert-service/handler-dependencies.ts
+++ src/lambdas/issue-reader-cert-service/handler-dependencies.ts
@@ -4,6 +4,7 @@ import {
   verifyAppCheckJwt,
   VerifyAppCheckJwtDependencies,
 } from './verify-app-check-jwt/verify-app-check-jwt.ts';
+import { issueCertificate, getCertificate } from './certificate-service.ts';
 
 export interface IssueReaderCertDependencies {
   env: NodeJS.ProcessEnv;
@@ -13,9 +14,13 @@ export interface IssueReaderCertDependencies {
     expectedJwtData: ExpectedAppCheckJwtData,
     dependencies?: VerifyAppCheckJwtDependencies,
   ) => Promise<Result<void>>;
+  issueCertificate: typeof issueCertificate;
+  getCertificate: typeof getCertificate;
 }
 
 export const dependencies: IssueReaderCertDependencies = {
   env: process.env,
   verifyAppCheckJwt,
+  issueCertificate,
+  getCertificate,
 };
