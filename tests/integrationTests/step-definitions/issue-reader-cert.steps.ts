c4b57b3a036d310b402d688d7c8fe3c9186b8562
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2026-04-21 15:51:11 +0100

    fix: update integration test step to have timeout value

--- tests/integrationTests/step-definitions/issue-reader-cert.steps.ts
+++ tests/integrationTests/step-definitions/issue-reader-cert.steps.ts
@@ -9,7 +9,7 @@ import type { HttpResponseSnapshot } from '../utils/api-instance.ts';
 
 let mockRequest: MockIssueReaderCertRequest | undefined;
 let response: HttpResponseSnapshot | undefined;
-let body;
+const LAMBDA_TIMEOUT = 60 * 15000;
 
 Before(() => {
   mockRequest = undefined;
@@ -20,14 +20,18 @@ Given('I generate a valid issue reader cert request', async () => {
   mockRequest = await requestMockIssueReaderCertRequest();
 });
 
-When('I submit the request to the issue reader cert endpoint', async () => {
-  assert.ok(
-    mockRequest,
-    'A mock issue reader certificate request must be generated first',
-  );
+When(
+  'I submit the request to the issue reader cert endpoint',
+  { timeout: LAMBDA_TIMEOUT },
+  async () => {
+    assert.ok(
+      mockRequest,
+      'A mock issue reader certificate request must be generated first',
+    );
 
-  response = await requestIssueReaderCert(mockRequest);
-});
+    response = await requestIssueReaderCert(mockRequest);
+  },
+);
 
 Then('the issue reader cert endpoint returns a 200 OK response', () => {
   assert.ok(
@@ -41,10 +45,13 @@ Then('the issue reader cert endpoint returns a 200 OK response', () => {
     `Unexpected response from ${response.url}: ${response.body}`,
   );
 
-  body = JSON.parse(response.body);
+  const parsedBody = JSON.parse(response.body);
 
-  assert.ok(typeof body.certChain === 'string', 'certChain should be a string');
-  assert.ok(body.certChain.length > 0, 'certChain should not be empty');
+  assert.ok(
+    typeof parsedBody.certChain === 'string',
+    'certChain should be a string',
+  );
+  assert.ok(parsedBody.certChain.length > 0, 'certChain should not be empty');
   assert.ok(response.headers['content-type']?.includes('application/json'));
   assert.ok(response.headers['x-request-id']?.trim());
 });
